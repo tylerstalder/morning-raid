1d31a562d2436942ea57c397a4597c830f36bcb3
Tyler Stalder <tyler@stalder.me>
2013-09-19 15:22:42 -0700

    Init Express, Angular, Twilio, Mailgun, Bower app

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+var app = angular.module('myApp', []);
+
+// "Main" method
+app.run(function($rootScope) {
+  $rootScope.name = "Tyler";
+});
+
+app.controller('MyController', function($scope) {
+  $scope.person = {
+    name: "Tyler"
+  };
+});
+
+var schedule = [
+  { date: '07/18/2013' },
+  { date: '07/25/2013' },
+  { date: '08/01/2013' },
+  { date: '08/08/2013' , attendees: ['mls']},
+  { date: '08/15/2013' , attendees: ['tjs','dbb','sms','mls','jorge','eric']},
+  { date: '08/22/2013' , attendees: []},
+  { date: '08/29/2013' , attendees: ['tjs','dbb','sms','mls','jorge','eric']},
+  { date: '09/05/2013' , attendees: ['tjs']}
+];
+
+var leaderboard = [
+  { name: 'Tyler', initials: 'tjs' },
+  { name: 'Tyler', initials: 'tjs' },
+  { name: 'Tyler', initials: 'tjs' },
+  { name: 'Tyler', initials: 'tjs' }
+];
+
+app.controller('Rides', function($scope) {
+  $scope.schedule = schedule;
+});
+
+app.controller('Leaderboard', function($scope) {
+  $scope.riders = leaderboard;
+});
+
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var mailgun = require('mailgun');
+var twilio = require('twilio')(process.env.TWILIO_KEY, process.env.TWILIO_SECRET);
+
+var Mailgun = require('mailgun').Mailgun;
+var mg = new Mailgun(process.env.MAILGUN_KEY);
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.twilio = function(req, res){
+  //Send an SMS text message
+  twilio.sendSms({
+    to: process.env.PHONE_TO_TEST,
+    from: process.env.PHONE_FROM_TEST,
+    body: 'Morning raid is tomorrow, you in?'
+  }, function(err, responseData) {
+    if (!err) {
+      console.log(err);
+      console.log(responseData.from);
+      console.log(responseData.body);
+    }
+  });
+  res.render('admin');
+};
+
+exports.email = function(req, res) {
+  mg.sendText(process.env.EMAIL_FROM_TEST, [process.env.EMAIL_TO_TEST],
+              'Morning Raid tomorrow',
+              'Spire at 7:15',
+              process.env.EMAIL_FROM_TEST, {},
+              function(err) {
+                if (err) console.log('Oh noes: ' + err);
+                else     console.log('Success');
+              });
+};
+
+exports.mailgun = function(req, res) {
+  console.log(req.body);
+};
+
+exports.twilio = function(req, res){
+  console.log(req.body);
+};
